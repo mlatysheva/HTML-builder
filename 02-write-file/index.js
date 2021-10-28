73470328d8fbc683a2038dcac2884b8cef9c319f
Maria-Latysheva <mlatysheva@gmail.com>
2021-10-28 08:59:52 +0200

    feat: complete the write functionality

---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const process = require('process');
+
+// create a path for the future input file and the file itself
+const resolvedPath = path.resolve('02-write-file', 'input.txt');
+
+let writeableStream = fs.createWriteStream(resolvedPath);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+ 
+function waitForUserInput() {
+  rl.question('Please input your text. To finish the input, press "Ctrl+c" or type "exit": ', function(answer) {
+    if (answer.includes("exit")) {
+
+      //write to a file minus last 4 characters;
+      writeableStream.write(answer.substring(0, answer.length - 4));      
+      rl.close();
+    } else {
+      writeableStream.write(answer);
+      waitForUserInput();
+    }
+    rl.on("close", function() {
+      console.log("\nGood-bye!");
+      process.exit(0);
+    });
+  });
+}
+waitForUserInput();
+
